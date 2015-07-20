c3231e68c77905216c97fc1879e139c279abeca0
Ktar5 <buildfresh@gmail.com>
2015-07-20 00:46:01 -0700

    Work on recipe loader..

--- src/main/java/com/minecave/powders/recipe/RecipeLoader.java
+++ src/main/java/com/minecave/powders/recipe/RecipeLoader.java
@@ -3,6 +3,7 @@ package com.minecave.powders.recipe;
 import com.minecave.powders.item.CustomItem;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,20 +15,25 @@ public class RecipeLoader {
 
     static private Map<String, CustomItem> items;
 
-    public static void loadRecipes(HashMap<String, CustomItem> itemz){
-            items = itemz;
+    public static Map<ShapedRecipe, CustomItem> loadRecipes(HashMap<String, CustomItem> itemz){
+        items = itemz;
+        Map<ShapedRecipe, CustomItem> itemMap = new HashMap<>();
+
         items = null;
     }
 
-    public static void loadRecipe(String[][] stringMatrix, String name){
+    public Recipe loadRecipe(String[][] stringMatrix, String name){
         int i = 0;
         Recipe recipe = new Recipe();
         for(String[] strings : stringMatrix){
             for(String string : strings){
                 if(string.toUpperCase().equals(string)){
-                    recipe.put(RecipeSpot.values()[i], new ItemStack(Material.getMaterial(string)));
+                    recipe.put(RecipeSpot.values()[i++], new ItemStack(Material.getMaterial(string)));
+                }else{
+                    recipe.put(RecipeSpot.values()[i++], items.get(name).getEndItem());
                 }
             }
         }
+        return recipe;
     }
 }
