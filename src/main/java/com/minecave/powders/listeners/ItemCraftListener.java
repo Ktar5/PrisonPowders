fb877291eb8f900d294d341d3b8d8862be700efd
Ktar5 <buildfresh@gmail.com>
2015-07-19 19:39:54 -0700

    Finished the item crafting listener

--- src/main/java/com/minecave/powders/listeners/ItemCraftListener.java
+++ src/main/java/com/minecave/powders/listeners/ItemCraftListener.java
@@ -1,9 +1,12 @@
 package com.minecave.powders.listeners;
 
+import com.minecave.powders.Powders;
+import com.minecave.powders.item.CustomItem;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.ShapedRecipe;
 
 /**
  * Created by Carter on 7/16/2015.
@@ -12,7 +15,16 @@ public class ItemCraftListener implements Listener {
 
     @EventHandler
     public void onItemCraft(PrepareItemCraftEvent event){
-        CraftingInventory inv = (CraftingInventory) event.getInventory();
+        CraftingInventory inv = event.getInventory();
+        if(event.getRecipe() instanceof ShapedRecipe){
+            ShapedRecipe recipe = (ShapedRecipe) event.getRecipe();
+            CustomItem item = Powders.getInstance().getItemCoordinator().getByRecipe(recipe);
+            if(item != null){
+                if(!item.getRecipe().compareTo(inv.getMatrix())){
+                    inv.setResult(null);
+                }
+            }
+        }
 
     }
 }
