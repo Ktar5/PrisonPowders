c5956070a05b634a0b88f4bad3cb90f5c7e80419
Ktar5 <buildfresh@gmail.com>
2015-07-20 23:00:18 -0700

    Finished Recipe Loader

--- src/main/java/com/minecave/powders/recipe/RecipeLoader.java
+++ src/main/java/com/minecave/powders/recipe/RecipeLoader.java
@@ -1,12 +1,16 @@
 package com.minecave.powders.recipe;
 
+import com.minecave.powders.Powders;
 import com.minecave.powders.item.CustomItem;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Created by Carter on 7/19/2015.
@@ -18,11 +22,21 @@ public class RecipeLoader {
     public static Map<ShapedRecipe, CustomItem> loadRecipes(HashMap<String, CustomItem> itemz){
         items = itemz;
         Map<ShapedRecipe, CustomItem> itemMap = new HashMap<>();
-
+        FileConfiguration config = Powders.getInstance().getItems().getConfig();
+        for(Map.Entry<String, CustomItem> entry : items.entrySet()){
+            List<String> list = config.getStringList(entry.getKey() + " .recipe");
+            String[][] stringMatrix = new String[3][3];
+            for(int i = 0 ; i < list.size() ; i++){
+                stringMatrix[i] = list.get(i).split(Pattern.quote(","));
+            }
+            Recipe recipe = loadRecipe(stringMatrix, entry.getKey());
+            itemMap.put(recipe.toShapedRecipe(entry.getValue().getEndItem()), entry.getValue());
+        }
         items = null;
+        return itemMap;
     }
 
-    public Recipe loadRecipe(String[][] stringMatrix, String name){
+    public static Recipe loadRecipe(String[][] stringMatrix, String name){
         int i = 0;
         Recipe recipe = new Recipe();
         for(String[] strings : stringMatrix){
